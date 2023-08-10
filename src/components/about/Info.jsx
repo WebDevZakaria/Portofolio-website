a95deb66b56bc18e1ded96c4125b6d12dcd813ab
zaki <zikoubouregbi@gmail.com>
2023-08-10 16:42:15 +0100

    final commit

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -6,13 +6,13 @@ function Info() {
         <div className="about__box">
          <i class='bx bx-award'></i>
          <h3 className="about__title">Experiences</h3>
-         <span className="about__subtitle"> 2 years working with django and react js  </span>
+         <span className="about__subtitle"> 3 years working with django and react js  </span>
         </div>
 
         <div className="about__box">
         <i class='bx bx-briefcase-alt' ></i>
             <h3 className="about__title">Recent Projects</h3>
-            <span className="about__subtitle"> 7 project </span>
+            <span className="about__subtitle"> 8 project </span>
             
         </div>
 
